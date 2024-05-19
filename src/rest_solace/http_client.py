ab44c61ff0d18428a4a37a7d8b94851f74bc9d55
skyler-guha <104735259+skyler-guha@users.noreply.github.com>
2024-05-19 21:32:54 +0530

    Added 2 async functions into the http client (this is still experimental).

--- src/rest_solace/http_client.py
+++ src/rest_solace/http_client.py
@@ -1,10 +1,9 @@
-"""
-module for http client
-"""
 import json
 import requests
 from requests.auth import HTTPBasicAuth
-
+import aiohttp
+from aiohttp import BasicAuth
+#import asyncio
 
 class HttpClient:
     """class to make http/https requests"""
@@ -18,13 +17,14 @@ class HttpClient:
         self.user_name = user_name
         self.password = password
         self.authHeader = HTTPBasicAuth(self.user_name, self.password)
+        self.authHeaderAsync = BasicAuth(self.user_name, self.password)
         
         self.content_type_headers = {"json": {'Content-Type': 'application/json'},
                                      "plain_text": {'Content-Type': 'text/plain'},
                                      "binary": {'Content-Type':'application/octet-stream'}}
 
 
-    def http_get(self, endpoint: str, headers:dict= None):
+    def http_get(self, endpoint: str, headers:dict= None, timeout=None):
         """method to get the http endpoint
         Args:
             endpoint: endpoint string
@@ -35,30 +35,15 @@ class HttpClient:
         """
         url = f"{self.base_url}{endpoint}"
 
-        req = requests.get(url, auth= self.authHeader, 
-                           headers= headers,
-                           verify= self.verify_ssl)
-        return req
-
-
-    def http_patch(self, endpoint:str, payload, headers:dict= {'Content-Type': 'application/json'}):
-        """method to update the http endpoint
-        Args:
-            endpoint: endpoint string
-            payload: request payload
-
-        Raises:
-            HTTP PATCH request failed. with response status code or
-            HTTP error occurred while HTTP PATCH exception
-        """
-        url = f"{self.base_url}{endpoint}"
+        return requests.get(url, auth= self.authHeader, 
+                            headers= headers,
+                            verify= self.verify_ssl,
+                            timeout= timeout)
         
-        req = requests.patch(url, auth=self.authHeader, data=json.dumps(payload),
-                                headers=headers, verify=self.verify_ssl)
-        return req
 
 
-    def http_post(self, endpoint: str, payload:dict|str, headers:dict= {'Content-Type': 'application/json'}, timeout=None):
+    def http_post(self, endpoint: str, payload:dict|str, 
+                  headers:dict= {'Content-Type': 'application/json'}, timeout=None):
         """method for http post
         Args:
             endpoint: endpoint string
@@ -71,9 +56,32 @@ class HttpClient:
 
         url = f"{self.base_url}{endpoint}"
     
-        req = requests.post(url, auth=self.authHeader, data=json.dumps(payload),
-                            headers=headers, verify=self.verify_ssl, timeout= timeout)
-        return req
+        return requests.post(url= url, 
+                             auth=self.authHeader, 
+                             data=json.dumps(payload),
+                             headers=headers, 
+                             verify=self.verify_ssl, 
+                             timeout= timeout)
+        
+
+    def http_patch(self, endpoint:str, payload, headers:dict= {'Content-Type': 'application/json'}, timeout=None):
+        """method to update the http endpoint
+        Args:
+            endpoint: endpoint string
+            payload: request payload
+
+        Raises:
+            HTTP PATCH request failed. with response status code or
+            HTTP error occurred while HTTP PATCH exception
+        """
+        url = f"{self.base_url}{endpoint}"
+        
+        return requests.patch(url= url, 
+                              auth= self.authHeader, 
+                              data= json.dumps(payload),
+                              headers= headers, 
+                              verify= self.verify_ssl, 
+                              timeout= timeout)
 
 
     def http_delete(self, endpoint:str, headers:dict= {'Content-Type': 'application/json'}):
@@ -87,8 +95,50 @@ class HttpClient:
         """
         url = f"{self.base_url}{endpoint}"
         
-        req = requests.delete(url, auth=self.authHeader, headers=headers,
-                              verify=self.verify_ssl)
+        return requests.delete(url= url, 
+                               auth= self.authHeader, 
+                               headers= headers,
+                               verify= self.verify_ssl)
     
-        return req
+
+    async def async_http_get(self, endpoint: str, headers:dict= None, timeout=None):
+        """async method to get the http endpoint
+        Args:
+            endpoint: endpoint string
+
+        Raises:
+            HTTP GET request failed. with response status code or
+            HTTP error occurred while HTTP GET exception
+        """
+        url = f"{self.base_url}{endpoint}"
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=self.verify_ssl),
+                                         auth= self.authHeaderAsync, 
+                                         headers= headers,
+                                         timeout= timeout) as session:
+
+            return await session.get(url= url)
+        
+    async def async_http_post(self, endpoint: str, payload:dict|str, 
+                        headers:dict= {'Content-Type': 'application/json'}, timeout=None):
+        """async method for http post
+        Args:
+            endpoint: endpoint string
+            payload: request payload
+
+        Raises:
+            HTTP POST request failed. with response status code or
+            HTTP error occurred while HTTP POST exception
+        """
+
+        url = f"{self.base_url}{endpoint}"
+
+        async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(verify_ssl=self.verify_ssl),
+                                         auth= self.authHeaderAsync, 
+                                         headers= headers,
+                                         timeout= timeout) as session:
+
+            return await session.post(url= url, data= json.dumps(payload))
+    
+
     
