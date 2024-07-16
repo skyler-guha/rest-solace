ee8ceffc38b3cb462f0dc02c52bc949a8c3fc518
skyler-guha <104735259+skyler-guha@users.noreply.github.com>
2024-07-16 20:50:54 +0530

    updated publisher tests.

--- tests/publisher_test.py
+++ tests/publisher_test.py
@@ -7,6 +7,7 @@ import asyncio
 import pprint
 import logging
 logging.basicConfig(level=logging.WARNING)
+import requests
 
 SEMP_HOST= "172.17.0.1"
 SEMP_PORT= "8080"
@@ -18,9 +19,6 @@ NEW_VPN_PORT=  6969
 
 CONSUMER_HOST= "127.0.0.1"
 CONSUMER_PORT= 5000
-
-INPUT_MESSAGE= "hello world!!"
-EXPECTED_OUTPUT_MESSAGE= bytes("HELLO WORLD!!", 'utf-8')
 
 manager = Manager(user_name= SEMP_USERNAME, 
                       password= SEMP_PASSWORD, 
@@ -34,27 +32,30 @@ consumer_obj = Consumer()
 
 def setup():
 
-    manager.create_message_vpn(msgVpnName= NEW_VPN_NAME,
-                           serviceRestIncomingPlainTextListenPort= NEW_VPN_PORT)
-
-    manager.auto_rest_messaging_setup_utility(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer', subscriptionTopic='my_topic', 
-                                              restDeliveryPointName='myRDP', restConsumerName='myConsumer', 
-                                              remoteHost= CONSUMER_HOST, remotePort= CONSUMER_PORT)
-        
-    args = ("127.0.0.1", 5000, return_uppercase, True, True, 10)
+    args = ("127.0.0.1", 5000, return_uppercase, False, False)
 
     consumer_thread = threading.Thread(target= consumer_obj.startConsumer, 
                                        args= args)
     consumer_thread.start()
 
+    
+    manager.create_message_vpn(msgVpnName= NEW_VPN_NAME,
+                           serviceRestIncomingPlainTextListenPort= NEW_VPN_PORT)
+
+    manager.auto_rest_messaging_setup_utility(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer', subscriptionTopic='my_topic', 
+                                              restDeliveryPointName='myRDP', restConsumerName='myConsumer', 
+                                              remoteHost= CONSUMER_HOST, remotePort= CONSUMER_PORT)    
+
     manager.restart_rest_delivery_point(restDeliveryPointName= "myRDP", 
                                         msgVpnName= NEW_VPN_NAME)
+    
 
 
 def teardown():
     manager.delete_queue_endpoint(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer')
     manager.delete_rest_delivery_point(msgVpnName= NEW_VPN_NAME, restDeliveryPointName= 'myRDP')
     manager.delete_message_vpn(msgVpnName= NEW_VPN_NAME)
+    requests.post(f"http://{CONSUMER_HOST}:{str(CONSUMER_PORT)}", data= "kill_server")
 
 
 #Sync functions
@@ -63,7 +64,7 @@ def test_direct_message_to_queue():
     setup()
     try:
         res = publish.direct_message_to_queue(queue_name= "queue_rest_consumer",
-                                              message= INPUT_MESSAGE,
+                                              message= "direct_message_to_queue",
                                               timeout=5)
         print("\nResponse:\n",res)
     except Exception as e:
@@ -75,7 +76,7 @@ def test_direct_message_for_topic():
     setup()
     try:
         res = publish.direct_message_for_topic(topic_string= "my_topic",
-                                               message= INPUT_MESSAGE,
+                                               message= "direct_message_for_topic",
                                                timeout=5)
         print("\nResponse:\n",res)
     except Exception as e:
@@ -87,7 +88,7 @@ def test_persistent_message_to_queue():
     setup()
     try:
         res = publish.persistent_message_to_queue(queue_name= "queue_rest_consumer",
-                                                  message= INPUT_MESSAGE,
+                                                  message= "persistent_message_to_queue",
                                                   timeout=5,
                                                   request_reply= True)
         print("\nResponse:\n",res)
@@ -100,7 +101,7 @@ def test_persistent_message_for_topic():
     setup()
     try:
         res = publish.persistent_message_for_topic(topic_string= "my_topic",
-                                                   message= INPUT_MESSAGE,
+                                                   message= "persistent_message_for_topic",
                                                    timeout=5,
                                                    request_reply= True)
         print("\nResponse:\n",res)
@@ -114,7 +115,7 @@ async def test_async_direct_message_to_queue():
     setup()
     try:
         res = await publish.async_direct_message_to_queue(queue_name= "queue_rest_consumer",
-                                                          message= INPUT_MESSAGE,
+                                                          message= "async_direct_message_to_queue",
                                                           timeout=5)
         print("\nResponse:\n",res)
     except Exception as e:
@@ -126,7 +127,7 @@ async def test_async_direct_message_for_topic():
     setup()
     try:
         res = await publish.async_direct_message_for_topic(topic_string= "my_topic",
-                                                            message= INPUT_MESSAGE,
+                                                            message= "async_direct_message_for_topic",
                                                             timeout=5)
         print("\nResponse:\n",res)
     except Exception as e:
@@ -138,7 +139,7 @@ async def test_async_persistent_message_to_queue():
     setup()
     try:
         res = await publish.async_persistent_message_to_queue(queue_name= "queue_rest_consumer",
-                                                              message= INPUT_MESSAGE,
+                                                              message= "async_persistent_message_to_queue",
                                                               timeout=5,
                                                               request_reply= True)
         print("\nResponse:\n",res)
@@ -151,7 +152,7 @@ async def test_async_persistent_message_for_topic():
     setup()
     try:
         res = await publish.async_persistent_message_for_topic(topic_string= "my_topic",
-                                                               message= INPUT_MESSAGE,
+                                                               message= "async_persistent_message_for_topic",
                                                                timeout=5,
                                                                request_reply= True)
         print("\nResponse:\n",res)
@@ -161,48 +162,50 @@ async def test_async_persistent_message_for_topic():
 
 def test_send_messages():
     print("\nTesting Function: 'send_messages'")
-    #setup()
+    setup()
 
-    manager.create_message_vpn(msgVpnName= NEW_VPN_NAME,
-                           serviceRestIncomingPlainTextListenPort= NEW_VPN_PORT)
+    # manager.create_message_vpn(msgVpnName= NEW_VPN_NAME,
+    #                        serviceRestIncomingPlainTextListenPort= NEW_VPN_PORT)
 
-    manager.auto_rest_messaging_setup_utility(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer', subscriptionTopic='my_topic', 
-                                              restDeliveryPointName='myRDP', restConsumerName='myConsumer', 
-                                              remoteHost= CONSUMER_HOST, remotePort= CONSUMER_PORT)
+    # manager.auto_rest_messaging_setup_utility(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer', subscriptionTopic='my_topic', 
+    #                                           restDeliveryPointName='myRDP', restConsumerName='myConsumer', 
+    #                                           remoteHost= CONSUMER_HOST, remotePort= CONSUMER_PORT)
 
     try:
+        print("sending messages")
         res = publish.send_messages(data= "test_data.json")
-        print("\nResponse:\n")
+        print("\nResponse:")
         pprint.pprint(res)
     except Exception as e:
         print(e)
 
-    manager.delete_queue_endpoint(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer')
-    manager.delete_rest_delivery_point(msgVpnName= NEW_VPN_NAME, restDeliveryPointName= 'myRDP')
-    manager.delete_message_vpn(msgVpnName= NEW_VPN_NAME)
-    #teardown()
+    # manager.delete_queue_endpoint(msgVpnName= NEW_VPN_NAME, queueName= 'queue_rest_consumer')
+    # manager.delete_rest_delivery_point(msgVpnName= NEW_VPN_NAME, restDeliveryPointName= 'myRDP')
+    # manager.delete_message_vpn(msgVpnName= NEW_VPN_NAME)
+    time.sleep(2)
+    teardown()
 
 #Running all sync tests
-# test_direct_message_to_queue()
-# time.sleep(10)
-# test_direct_message_for_topic()
-# time.sleep(10)
-# test_persistent_message_to_queue()
-# time.sleep(10)
-# test_persistent_message_for_topic()
-# time.sleep(10)
+test_direct_message_to_queue()
+time.sleep(5)
+test_direct_message_for_topic()
+time.sleep(5)
+test_persistent_message_to_queue()
+time.sleep(5)
+test_persistent_message_for_topic()
+time.sleep(5)
 
-# #Running all async tests
-# asyncio.run(test_async_direct_message_to_queue())
-# time.sleep(15)
-# asyncio.run(test_async_direct_message_for_topic())
-# time.sleep(15)
-# asyncio.run(test_async_persistent_message_to_queue())
-# time.sleep(15)
-# asyncio.run(test_async_persistent_message_for_topic())
+#Running all async tests
+asyncio.run(test_async_direct_message_to_queue())
+time.sleep(5)
+asyncio.run(test_async_direct_message_for_topic())
+time.sleep(5)
+asyncio.run(test_async_persistent_message_to_queue())
+time.sleep(5)
+asyncio.run(test_async_persistent_message_for_topic())
 
 #testing sending multiple messages
-time.sleep(15)
+time.sleep(5)
 test_send_messages()
 
 
